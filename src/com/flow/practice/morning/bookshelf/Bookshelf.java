280ca8e0f1e16da3848a70098ae05cd36ab26d31
Szabina Hazi <szabina.hazi@flowacademy.hu>
2018-05-04 16:22:13 +0200

    refactored

--- src/com/flow/practice/morning/bookshelf/Bookshelf.java
+++ src/com/flow/practice/morning/bookshelf/Bookshelf.java
@@ -2,119 +2,173 @@ package com.flow.practice.morning.bookshelf;
 
 import com.flow.practice.morning.book.*;
 import java.util.ArrayList;
+import java.util.HashMap;
+
+// Novel Egri_csillagok,Gárdonyi_Géza,Móra,1901 Textbook Kalkulus1,Toth_Istvan,Tankönyv_kiadó,1997 Novel Alas_Babylon,Pat_Frank,Lippincott,1959,Post-apocalyptic
 
 public class Bookshelf {
 
-    static ArrayList<Book> myBookshelf = new ArrayList<Book>();
+    private String[] arguments;
+    private ArrayList<Book> myBookshelf = new ArrayList<>();
+    private int iterator = 0;
 
-    // Regeny Egri_csillagok,Gárdonyi_Géza,Móra,1901 Tankonyv Kalkulus1,Toth_Istvan,Tankönyv_kiadó,1997 Regeny Alas_Babylon,Pat_Frank,Lippincott,1959,Post-apocalyptic
+    private void setArguments(String[] array) {
+        arguments = array;
+    }
+
+    private boolean isTextbook() {
+        return arguments[iterator].equals("Textbook");
+    }
+
+    private boolean isNovel() {
+        return arguments[iterator].equals("Novel");
+    }
+
+    private boolean hasParameters() {
+        return (!(arguments[iterator + 1]).equals("Novel") || arguments[iterator + 1].equals("Textbook"));
+    }
+
+    private boolean has5thParameter() {
+        return (arguments[iterator].split(",").length == 5);
+    }
+
+    private String[] prepareArguments() {
+        return arguments[iterator + 1].split(",");
+    }
+
+    private HashMap<String, Object> getOptions() {
+        HashMap<String, Object> options = new HashMap<>();
+        String[] properties = prepareArguments();
+
+        options.put("title", gettingTitle(properties[0]));
+        options.put("author", gettingAuthor(properties[1]));
+        options.put("publisher", gettingPublisher(properties[2]));
+        options.put("date", gettingDate(properties[3]));
+
+        if (isTextbook() && has5thParameter()) {
+            options.put("subject", getting5thParameter(properties[4]));
+        } else if (isNovel() && has5thParameter()) {
+            options.put("genre", getting5thParameter(properties[4]));
+        }
+
+        return options;
+    }
+
+    private String gettingTitle(String titleString) {
+        if (titleString.contains("_")) {
+            String title = "";
+            String[] titleStringSplit = titleString.split("_");
+            for (String titleElement : titleStringSplit) {
+                title += titleElement + " ";
+            }
+            return title.trim();
+        } else {
+            return titleString;
+        }
+    }
+
+    private String gettingAuthor(String authorString) {
+        if (authorString.contains("_")) {
+            String author = "";
+            String[] authorStringSplit = authorString.split("_");
+            for (String titleElement : authorStringSplit) {
+                author += titleElement + " ";
+            }
+            return author.trim();
+        } else {
+            return authorString;
+        }
+    }
+
+    private String gettingPublisher(String publisherString) {
+        if (publisherString.contains("_")) {
+            String publisher = "";
+            String[] publisherStringSplit = publisherString.split("_");
+            for (String publisherElement : publisherStringSplit) {
+                publisher += publisherElement + " ";
+            }
+            return publisher.trim();
+        } else {
+            return publisherString;
+        }
+    }
+
+    private int gettingDate (String dateString) {
+        return Integer.parseInt(dateString);
+    }
+
+    private String getting5thParameter(String paramString) {
+        if (paramString.contains("_")) {
+            String parameter = "";
+            String[] paramStringSplit = paramString.split("_");
+            for (String paramElement : paramStringSplit) {
+                parameter += paramElement + " ";
+            }
+            return parameter.trim();
+        } else {
+            return paramString;
+        }
+    }
+
+
+    private void createDefaultBook() {
+        myBookshelf.add(new Book());
+        iterator += 1;
+    }
+
+    private void createTextbook() {
+        HashMap<String, Object> options = getOptions();
+        if (has5thParameter()) {
+          myBookshelf.add(new Textbook((String) options.get("title"), (String) options.get("author"), (String) options.get("publisher"),
+                  (int) options.get("date"), (String) options.get("subject")));
+        } else {
+         myBookshelf.add(new Textbook((String) options.get("title"), (String) options.get("author"), (String) options.get("publisher"),
+                 (int) options.get("date")));
+        }
+        iterator += 2;
+    }
+
+    private void createNovel() {
+        HashMap<String, Object> options = getOptions();
+        if (has5thParameter()) {
+            myBookshelf.add(new Novel((String) options.get("title"), (String) options.get("author"), (String) options.get("publisher"),
+                    (int) options.get("date"), (String) options.get("genre")));
+        } else {
+            myBookshelf.add(new Novel((String) options.get("title"), (String) options.get("author"), (String) options.get("publisher"),
+                    (int) options.get("date")));
+        }
+        iterator += 2;
+    }
+
+    private void createBook() {
+        if (hasParameters()) {
+            if (isTextbook()) {
+                createTextbook();
+            }
+            else {
+                createNovel();
+            }
+        } else {
+          createDefaultBook();
+        }
+    }
+
+    public void iterate() {
+        while (iterator < arguments.length) {
+            createBook();
+        }
+    }
 
     public static void main(String[] args) {
 
-        for (int i = 0; i < args.length; i++) {
-            if (args[i].equals("Regeny")) {
-                String[] bookProperties = args[i + 1].split(",");
+        Bookshelf bookshelf = new Bookshelf();
+        bookshelf.setArguments(args);
+        bookshelf.iterate();
 
-                // title
-                String[] title = bookProperties[0].split("_");
-                String titleString = "";
-                for (int j = 0; j < title.length; j++) {
-                    titleString += title[j] + " ";
-                }
-                titleString = titleString.trim();
 
-                // author
-                String[] author = bookProperties[1].split("_");
-                String authorString = "";
-                for (int k = 0; k < author.length; k++) {
-                    authorString += author[k] + " ";
-                }
-                authorString = authorString.trim();
-
-                // publisher
-                String[] publisher = bookProperties[2].split("_");
-                String publisherString = "";
-                for (int l = 0; l < publisher.length; l++) {
-                    publisherString += publisher[l] + " ";
-                }
-                publisherString = publisherString.trim();
-
-                // date
-                int datePub = Integer.parseInt(bookProperties[3]);
-
-                if (bookProperties.length == 5) {
-                    // genre, if custom
-                    String[] genre = bookProperties[4].split("_");
-                    String genreString = "";
-                    for (int m = 0; m < genre.length; m++) {
-                        genreString += genre[m] + " ";
-                    }
-                    genreString = genreString.trim();
-
-                    Novel novel = new Novel(titleString, authorString, publisherString, datePub, genreString);
-                    putOnBookshelf(myBookshelf, novel);
-                } else {
-                    Novel novel = new Novel(titleString, authorString, publisherString, datePub);
-                    putOnBookshelf(myBookshelf, novel);
-                }
-                i += 1;
-            }
-            if (args[i].equals("Tankonyv")) {
-                String[] bookProperties = args[i + 1].split(",");
-
-                // title
-                String[] title = bookProperties[0].split("_");
-                String titleString = "";
-                for (int j = 0; j < title.length; j++) {
-                    titleString += title[j] + " ";
-                }
-                titleString = titleString.trim();
-
-                // author
-                String[] author = bookProperties[1].split("_");
-                String authorString = "";
-                for (int k = 0; k < author.length; k++) {
-                    authorString += author[k] + " ";
-                }
-                authorString = authorString.trim();
-
-                // publisher
-                String[] publisher = bookProperties[2].split("_");
-                String publisherString = "";
-                for (int l = 0; l < publisher.length; l++) {
-                    publisherString += publisher[l] + " ";
-                }
-                publisherString = publisherString.trim();
-
-                int datePub = Integer.parseInt(bookProperties[3]);
-
-                if (bookProperties.length == 5) {
-                    // genre, if custom
-                    String[] subject = bookProperties[4].split("_");
-                    String subjectString = "";
-                    for (int m = 0; m < subject.length; m++) {
-                        subjectString += subject[m] + " ";
-                    }
-                    subjectString = subjectString.trim();
-                    Textbook textbook = new Textbook(titleString, authorString, publisherString, datePub, subjectString);
-                    putOnBookshelf(myBookshelf, textbook);
-                } else {
-                    Textbook textbook = new Textbook(titleString, authorString, publisherString, datePub);
-                    putOnBookshelf(myBookshelf, textbook);
-                }
-                i += 1;
-            }
+        for (Book book : bookshelf.myBookshelf) {
+            System.out.println(book);
         }
-
-        for (int i = 0; i < myBookshelf.size(); i ++) {
-            System.out.println(myBookshelf.get(i));
-        }
-        System.out.println("Number of novels on the shelf: " + Novel.getNovelCounter());
-
     }
 
-    public static ArrayList<Book> putOnBookshelf(ArrayList<Book> bookshelf, Book book) {
-        bookshelf.add(book);
-        return bookshelf;
-    }
 }
